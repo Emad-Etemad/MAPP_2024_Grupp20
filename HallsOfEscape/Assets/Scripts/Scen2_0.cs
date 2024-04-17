aafbcb115ce703623bda1387a2068d88771c5c69
chloedao <chloedaoo@gmail.com>
2024-04-17 14:20:27 +0200

    Scen2.0

---
+++ HallsOfEscape/Assets/Scripts/Scen2_0.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scen2_0 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(3); // Ladda nästa scen
+    }
+
+}
