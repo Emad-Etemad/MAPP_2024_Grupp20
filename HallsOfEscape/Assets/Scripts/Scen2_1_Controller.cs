a238470b1ef38f63e5dd4c3d6c19372911e53ad2
Emad-Etemad <emadnikoo1998@gmail.com>
2024-04-19 15:35:37 +0200

    Scene 2.1

--- HallsOfEscape/Assets/Scripts/Scen2_1_Controller.cs
+++ HallsOfEscape/Assets/Scripts/Scen2_1_Controller.cs
@@ -1,23 +1,61 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;  // Inkludera detta namespace för TextMesh Pro
 
-public class Scen2_1_Controller : MonoBehaviour
+public class Scene2_1_Controller : MonoBehaviour
 {
+    public GameObject inactiveImage;  // Referens till GameObject med inaktiv bild
+    public TextMeshProUGUI timerText;  // Referens till TextMeshPro-objektet
+    private float countdown = 10f;  // Startvärde för nedräkningen
 
-    public void GoUpstairs()
+    void Start()
     {
+        StartCoroutine(CountdownTimer());
+    }
 
-        SceneManager.LoadScene(6);
+    IEnumerator CountdownTimer()
+    {
+        while (countdown > 0)
+        {
+            countdown -= Time.deltaTime;  // Minska countdown med tiden som gått sedan senaste frame
+            UpdateTimerDisplay(countdown);  // Uppdatera textdisplayen
+            yield return null;
+        }
 
+        SceneManager.LoadScene(0);  // Ladda om scen 0 när timern når 0
+    }
+
+    void UpdateTimerDisplay(float currentTime)
+    {
+        timerText.text = Mathf.Ceil(currentTime).ToString();  // Visa avrundat till närmaste heltal uppåt
+
+        // Ändra textfärg till röd under de sista 3 sekunderna, annars svart
+        if (currentTime <= 3)
+        {
+            timerText.color = Color.red;
+        }
+        else
+        {
+            timerText.color = Color.white;
+        }
+    }
+
+    // Metod för att hantera knapptryck
+    public void OnButtonPressed()
+    {
+        StartCoroutine(ShowInactiveImageAndSwitchScene());
+    }
+
+    IEnumerator ShowInactiveImageAndSwitchScene()
+    {
+        inactiveImage.SetActive(true);  // Aktiverar den inaktiva bilden
+        yield return new WaitForSeconds(3);  // Vänta 3 sekunder
+        SceneManager.LoadScene(0);  // Byt till scen 0
     }
 
     public void OpenDoor()
     {
-
-        SceneManager.LoadScene(7);
-
+        SceneManager.LoadScene(6);  // Ladda önskad scen
     }
-
 }
