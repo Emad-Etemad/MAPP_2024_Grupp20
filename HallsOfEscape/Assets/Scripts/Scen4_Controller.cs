72ba93442683398078e9b8172365aa3f2e8536ba
Najifamiah <najifamiah1@gmail.com>
2024-04-21 01:19:36 +0200

    Scen 4 övergång till scen 3, ChoiceOne

---
+++ HallsOfEscape/Assets/Scripts/Scen4_Controller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scen4_Controller : MonoBehaviour
+{
+    public void ChoiceOne()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
