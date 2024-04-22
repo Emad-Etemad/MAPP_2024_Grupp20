16cc56d5e6d41f79ab7de85a57481567a20b7b70
Emad-Etemad <emadnikoo1998@gmail.com>
2024-04-22 21:43:46 +0200

    PausPlay

--- HallsOfEscape/Assets/Scripts/MainMenu.cs
+++ HallsOfEscape/Assets/Scripts/MainMenu.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject pauseMenuUI;   // Referens till pausmenyn
+    public GameObject playButton;    // Referens till Play-knappen för att återuppta spelet
+
+    void Start()
+    {
+        playButton.SetActive(false); // Göm Play-knappen när spelet startar
+    }
 
     public void LoadScene()
     {
@@ -15,4 +22,29 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(0); // Ladda nästa scen
     }
 
+    public void TogglePause()
+    {
+        bool isPaused = pauseMenuUI.activeSelf;
+
+        if (!isPaused)
+        {
+            Time.timeScale = 0; // Pausa spelet
+            pauseMenuUI.SetActive(true); // Visa pausmenyn
+            playButton.SetActive(false); // Visa Play-knappen
+        }
+        else
+        {
+            Time.timeScale = 1; // Återuppta spelet
+            pauseMenuUI.SetActive(false); // Göm pausmenyn
+            playButton.SetActive(true); // Göm Play-knappen
+        }
+    }
+
+    public void PlayGame()
+    {
+        // Kallas när Play-knappen trycks
+        Time.timeScale = 1; // Återuppta spelet
+        pauseMenuUI.SetActive(true); // Göm pausmenyn
+        playButton.SetActive(false); // Göm Play-knappen
+    }
 }
