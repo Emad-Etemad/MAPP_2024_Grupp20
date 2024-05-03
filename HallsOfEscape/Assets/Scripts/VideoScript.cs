f4002a7c4c115645a1986ac082b63614eb90eaf3
Najifamiah <najifamiah1@gmail.com>
2024-05-03 17:29:46 +0200

    Menu update

--- HallsOfEscape/Assets/Scripts/VideoScript.cs
+++ HallsOfEscape/Assets/Scripts/VideoScript.cs
@@ -7,23 +7,47 @@ using UnityEngine.Video;
 public class VideoScript : MonoBehaviour
 {
 
-    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private VideoPlayer[] videoPlayer;
     [SerializeField] GameObject camera;
+    public int index;
 
     public void Start()
     {
-        videoPlayer.targetCamera = camera.GetComponent<Camera>();
-        playVideo();
+        videoPlayer[0].targetCamera = camera.GetComponent<Camera>();
+        index = 0;
+        playVideo(0);
     }
 
-    public void playVideo()
+    public void playVideo(int i)
     {
-        videoPlayer.Play();
-        videoPlayer.loopPointReached += loopMethod;
+        videoPlayer[i].Play();
+        index = i;
+        videoPlayer[i].loopPointReached += loopMethod;
     }
 
     void loopMethod(VideoPlayer vp)
     {
-        playVideo();
+        if (index == 0)
+            playVideo(index);
+        else if (index == 1)
+            SceneManager.LoadScene(3);
+        else if (index == 2)
+            SceneManager.LoadScene(5);
+    }
+
+    public void choice2()
+    {
+        videoPlayer[0].Pause();
+        videoPlayer[1].targetCamera = camera.GetComponent<Camera>();
+        index = 1;
+        playVideo(index);
+    }
+
+    public void choice3()
+    {
+        videoPlayer[0].Pause();
+        videoPlayer[1].targetCamera = camera.GetComponent<Camera>();
+        index = 1;
+        playVideo(index);
     }
 }
