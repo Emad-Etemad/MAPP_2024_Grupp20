f35a3dcbd2f32c509fa9a4dbd59dbb52c46fcc9a
Filippa Axelsson <filippaax123@gmail.com>
2024-05-13 12:40:05 +0200

    Ending4_readmore button

--- HallsOfEscape/Assets/Scripts/EndScene4.cs
+++ HallsOfEscape/Assets/Scripts/EndScene4.cs
@@ -6,13 +6,26 @@ using UnityEngine.Video;
 
 public class EndScene4 : MonoBehaviour
 {
-
+    [SerializeField] private GameObject readMoreInfoPanel;
+    [SerializeField] private AudioClip returnToMenuSound, readMorePanelSound, closeReadMorePanelSound;
+    private AudioSource audioSource;
 
 
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
-        Time.timeScale = 1f;
+        audioSource.PlayOneShot(returnToMenuSound, 0.5f);
     }
+    public void ShowReadMorePanel()
+    {
+        readMoreInfoPanel.SetActive(true);
+        audioSource.PlayOneShot(readMorePanelSound, 0.5f);
+    }
+    public void closeReadMorePanel()
+    {
+        readMoreInfoPanel.SetActive(false);
+        audioSource.PlayOneShot(closeReadMorePanelSound, 0.5f);
+    }
+
 
 }
